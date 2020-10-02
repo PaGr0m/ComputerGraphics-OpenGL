030b1b7caa853bbb66e92f130d398af2888db8ef
Pavel Gromov <pagrom27@gmail.com>
2020-10-03 02:31:18 +0300

    Create rectangle with textures. Add stb library

--- assets/simple.fs
+++ assets/simple.fs
@@ -2,7 +2,17 @@
 
 out vec4 color;
 
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture1;
+uniform sampler2D ourTexture2;
+
 void main()
 {
-	color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    color = mix(
+        texture(ourTexture1, TexCoord),
+        texture(ourTexture2, vec2(TexCoord.x, 1 - TexCoord.y)),
+        0.2
+    );
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
+#define STB_IMAGE_IMPLEMENTATION
+
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <fmt/format.h>
+#include <stb_image.h>
+
 #include "shader.h"
 
 
@@ -9,22 +14,43 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 const GLuint WINDOW_WIDTH = 800;
 const GLuint WINDOW_HEIGHT = 600;
 
+void load_texture(const std::string &filepath) {
+    int texture_x, texture_y, channels;
+
+    unsigned char *data = stbi_load(filepath.c_str(), &texture_x, &texture_y, &channels, 0);
+    if (data) {
+        std::string extension = filepath.substr(filepath.length() - 3);
+        if (extension == "png") {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture_x, texture_y, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        } else if (extension == "jpg") {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texture_x, texture_y, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        }
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cerr << fmt::format("Failed to load texture: {}", filepath);
+    }
+
+    stbi_image_free(data);
+}
+
 void init_buffers(GLuint &rectangleVBO, GLuint &rectangleVAO, GLuint &rectangleEBO) {
     GLfloat vertices[] = {
-            0.5f, 0.5f, 0.0f,   // Top Right
-            0.5f, -0.5f, 0.0f,  // Bottom Right
-            -0.5f, -0.5f, 0.0f, // Bottom Left
-            -0.5f, 0.5f, 0.0f   // Top Left
+            //      3        |        3        |            2
+            // Positions     |      Colors     |    Texture Coords
+            0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,     // Top Right
+            0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,    // Bottom Right
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,   // Bottom Left
+            -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f     // Top Left
     };
-
     GLuint indices[] = {
-            0, 1, 3,  // First Triangle
-            1, 2, 3   // Second Triangle
+            0, 1, 3, // First Triangle
+            1, 2, 3  // Second Triangle
     };
 
     glGenVertexArrays(1, &rectangleVAO);
     glGenBuffers(1, &rectangleVBO);
     glGenBuffers(1, &rectangleEBO);
+
     glBindVertexArray(rectangleVAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, rectangleVBO);
@@ -33,12 +59,41 @@ void init_buffers(GLuint &rectangleVBO, GLuint &rectangleVAO, GLuint &rectangleE
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, rectangleEBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *) nullptr);
+    // Positions
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *) nullptr);
     glEnableVertexAttribArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    // Color
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *) (3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    // Texture coordinate
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid *) (6 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
 
     glBindVertexArray(0);
+}
+
+void init_textures(GLuint &texture1, GLuint &texture2) {
+    // Texture 1
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    load_texture("assets/container.jpg");
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    // Texture 2
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    load_texture("assets/awesomeface.png");
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 int main() {
@@ -79,10 +134,13 @@ int main() {
     // Buffers
     GLuint rectangleVBO, rectangleVAO, rectangleEBO;
     init_buffers(rectangleVBO, rectangleVAO, rectangleEBO);
-    // ^^^ Buffers
 
+    // Textures
+    GLuint texture1, texture2;
+    init_textures(texture1, texture2);
+
+    // Shader
     Shader shader("assets/simple.vs", "assets/simple.fs");
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -90,10 +148,18 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        shader.set_uniform("ourTexture1", 0);
+
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        shader.set_uniform("ourTexture2", 1);
+
         shader.use();
 
         glBindVertexArray(rectangleVAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
         glBindVertexArray(0);
 
         glfwSwapBuffers(window);
