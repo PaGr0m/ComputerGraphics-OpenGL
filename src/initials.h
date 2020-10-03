71be1df050d3435faabec2bc3a39bc2e42438191
Pavel Gromov <pagrom27@gmail.com>
2020-10-03 18:45:33 +0300

    Add Sampler2D (TextureMap)

--- src/initials.h
+++ src/initials.h
@@ -52,48 +52,80 @@ void scroll_callback(GLFWwindow *window, double offset_x, double offset_y) {
     camera.process_mouse_scroll(offset_y);
 }
 
-void load_texture(const std::string &filepath) {
-    int texture_x, texture_y, channels;
+unsigned int load_texture(const std::string &filepath) {
+//    int texture_x, texture_y, channels;
+//
+//    unsigned char *data = stbi_load(filepath.c_str(), &texture_x, &texture_y, &channels, 0);
+//    if (data) {
+//        std::string extension = filepath.substr(filepath.length() - 3);
+//        if (extension == "png") {
+//            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture_x, texture_y, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+//        } else if (extension == "jpg") {
+//            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texture_x, texture_y, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+//        }
+//        glGenerateMipmap(GL_TEXTURE_2D);
+//    } else {
+//        std::cerr << fmt::format("Failed to load texture: {}", filepath);
+//    }
+//
+//    stbi_image_free(data);
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
 
-    unsigned char *data = stbi_load(filepath.c_str(), &texture_x, &texture_y, &channels, 0);
-    if (data) {
-        std::string extension = filepath.substr(filepath.length() - 3);
-        if (extension == "png") {
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture_x, texture_y, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        } else if (extension == "jpg") {
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texture_x, texture_y, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        }
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(filepath.c_str(), &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
-    } else {
-        std::cerr << fmt::format("Failed to load texture: {}", filepath);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    }
+    else
+    {
+        std::cout << "Texture failed to load at path: " << filepath << std::endl;
+        stbi_image_free(data);
     }
 
-    stbi_image_free(data);
+    return textureID;
 }
 
-void init_buffers(GLuint &VBO, GLuint &containerVAO, GLuint &lightVAO) {
-    glGenVertexArrays(1, &containerVAO);
+void init_buffers(GLuint &VBO, GLuint &cubeVAO, GLuint &lightVAO) {
+    glGenVertexArrays(1, &cubeVAO);
     glGenBuffers(1, &VBO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindVertexArray(containerVAO);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *) nullptr);
+    glBindVertexArray(cubeVAO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) nullptr);
     glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *) (3 * sizeof(GLfloat)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
     glBindVertexArray(0);
-
 
     glGenVertexArrays(1, &lightVAO);
     glBindVertexArray(lightVAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid *) nullptr);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) nullptr);
+    glEnableVertexAttribArray(0);
 
     glEnableVertexAttribArray(0);
     glBindVertexArray(0);
@@ -133,7 +165,7 @@ GLFWwindow *init_opengl() {
     GLFWwindow *window = glfwCreateWindow(
             WINDOW_WIDTH,
             WINDOW_HEIGHT,
-            "Lesson-7-materials",
+            "Lesson-8-texture-map",
             nullptr,
             nullptr
     );
--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@ int main() {
     glfwGetFramebufferSize(window, &frame_width, &frame_height);
 
     // Buffers
-    GLuint VBO, containerVAO, lightVAO;
-    init_buffers(VBO, containerVAO, lightVAO);
+    GLuint VBO, cubeVAO, lightVAO;
+    init_buffers(VBO, cubeVAO, lightVAO);
 
     // Textures
     GLuint texture1, texture2;
@@ -21,6 +21,16 @@ int main() {
     // Shader
     Shader lighting_shader("assets/cube.vs", "assets/cube.fs");
     Shader lamp_shader("assets/lamp.vs", "assets/lamp.fs");
+
+    // load textures (we now use a utility function to keep the code more organized)
+    unsigned int diffuseMap = load_texture("assets/container.png");
+    unsigned int specularMap = load_texture("assets/container_specular.png");
+
+    // shader configuration
+    // --------------------
+    lighting_shader.use();
+    lighting_shader.set_uniform("material.diffuse", 0);
+    lighting_shader.set_uniform("material.specular", 1);
 
     while (!glfwWindowShouldClose(window)) {
         // Set frame time
@@ -37,48 +47,46 @@ int main() {
 
         // Init lighting_shader
         lighting_shader.use();
-        lighting_shader.set_uniform("lightPos", lightPos);
+        lighting_shader.set_uniform("light.position", lightPos);
         lighting_shader.set_uniform("viewPos", camera.position());
 
         // Light properties
-        glm::vec3 lightColor = glm::vec3(0.7f,0.7f,0.7f );
-        lightColor.x = sin(glfwGetTime() * 2.0f);
-        lightColor.y = sin(glfwGetTime() * 0.7f);
-        lightColor.z = sin(glfwGetTime() * 1.3f);
-
-        glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f);
-        glm::vec3 ambientColor = diffuseColor * glm::vec3(0.25f);
-        lighting_shader.set_uniform("light.diffuse", diffuseColor);
-        lighting_shader.set_uniform("light.ambient", ambientColor);
+        lighting_shader.set_uniform("light.ambient", 0.2f, 0.2f, 0.2f);
+        lighting_shader.set_uniform("light.diffuse", 0.5f, 0.5f, 0.5f);
         lighting_shader.set_uniform("light.specular", 1.0f, 1.0f, 1.0f);
 
         // Material properties
-        lighting_shader.set_uniform("material.ambient", 1.0f, 0.5f, 0.31f);
-        lighting_shader.set_uniform("material.diffuse", 1.0f, 0.5f, 0.31f);
-        lighting_shader.set_uniform("material.specular", 0.5f, 0.5f, 0.5f);
-        lighting_shader.set_uniform("material.shininess", 32.0f);
+        lighting_shader.set_uniform("material.shininess", 64.0f);
 
         // Init MVP
         glm::mat4 model = glm::identity<glm::mat4>();
         glm::mat4 view = camera.view();
-        glm::mat4 projection = glm::perspective(camera.zoom(), RATIO, 0.1f, 100.0f);
+        glm::mat4 projection = glm::perspective(glm::radians(camera.zoom()), RATIO, 0.1f, 100.0f);
         lighting_shader.set_uniform("model", glm::value_ptr(model));
         lighting_shader.set_uniform("view", glm::value_ptr(view));
         lighting_shader.set_uniform("projection", glm::value_ptr(projection));
 
-        glBindVertexArray(containerVAO);
+        // Bind diffuse map
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, diffuseMap);
+
+        // Bind specular map
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, specularMap);
+
+        // Render the cube
+        glBindVertexArray(cubeVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
         // Init lamp_shader
-        lamp_shader.use();
-        lamp_shader.set_uniform("view", glm::value_ptr(view));
-        lamp_shader.set_uniform("projection", glm::value_ptr(projection));
-
         model = glm::identity<glm::mat4>();
         model = glm::translate(model, lightPos);
         model = glm::scale(model, glm::vec3(0.2f));
 
+        lamp_shader.use();
         lamp_shader.set_uniform("model", glm::value_ptr(model));
+        lamp_shader.set_uniform("view", glm::value_ptr(view));
+        lamp_shader.set_uniform("projection", glm::value_ptr(projection));
 
         glBindVertexArray(lightVAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -86,7 +94,7 @@ int main() {
         glfwSwapBuffers(window);
     }
 
-    glDeleteVertexArrays(1, &containerVAO);
+    glDeleteVertexArrays(1, &cubeVAO);
     glDeleteVertexArrays(1, &lightVAO);
     glDeleteBuffers(1, &VBO);
 
